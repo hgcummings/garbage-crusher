54dbf9755fdea0eb84692f31f7d2a510c6934a43
Joseph Walker <Joseph.Walker@softwire.com>
2022-04-03 14:12:50 +0100

    Display held item

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -3,41 +3,48 @@ extends KinematicBody2D
 var rng = RandomNumberGenerator.new()
 
 export var SPEED = 500
-export var random_throw_velocity = 2.0
+export var random_throw_velocity = 5.0
 export var random_throw_angular_velocity = 1.0
 
 var heldItem = null
+export var player_number = 1
 
 var inputDirection = Vector2.ZERO
 var movementVector = Vector2.ZERO
 
 func handle_pick_up():
 	heldItem = $RayCast2D.get_collider()
-	get_tree().get_root().remove_child(heldItem)
+	if heldItem == null:
+		return
+	get_tree().get_root().call_deferred("remove_child", heldItem)
+	
+	$HeldItemSprite.add_child(heldItem.get_node("Sprite").duplicate())
 
 func handle_drop():
-	heldItem.position = position
+	get_tree().get_root().call_deferred("add_child", heldItem)
+	$HeldItemSprite.get_node("Sprite").queue_free()
+	
+	heldItem.global_position = $HeldItemSprite.global_position
 	heldItem.angular_velocity = rng.randf_range(-random_throw_angular_velocity, random_throw_angular_velocity)
 	heldItem.linear_velocity = rng.randf_range(0, random_throw_velocity) * $RayCast2D.cast_to
 	if rng.randi_range(0, 20) == 0:
 		heldItem.linear_velocity = 20 * $RayCast2D.cast_to
 		
-	get_tree().get_root().add_child(heldItem)
 	heldItem = null
 
 func process_inputs():
 	# Process inputs
 	inputDirection = Vector2.ZERO
-	if Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP):
+	if Input.is_action_pressed("up_%s" % player_number):
 		inputDirection += Vector2.UP
-	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
+	if Input.is_action_pressed("down_%s" % player_number):
 		inputDirection += Vector2.DOWN
-	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
+	if Input.is_action_pressed("left_%s" % player_number):
 		inputDirection += Vector2.LEFT
-	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
+	if Input.is_action_pressed("right_%s" % player_number):
 		inputDirection += Vector2.RIGHT
 		
-	if Input.is_key_pressed(KEY_SPACE):
+	if Input.is_action_just_released("interact_%s" % player_number):
 		if heldItem == null:
 			handle_pick_up()
 		else:

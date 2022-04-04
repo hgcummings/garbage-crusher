26344a498c13982a7950a4a2a145e028d0822273
Emily F <emily@emilyfoxmusic.co.uk>
2022-04-04 17:12:57 +0100

    Menu screen

---
+++ StartMenu.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+func _ready():
+	$NewGame.color = Color.gainsboro
+	$Quit.color = Color.gainsboro
+
+func _on_NewGame_gui_input(event):
+	if event is InputEventMouseButton:
+		get_tree().change_scene("res://Main.tscn")
+
+func _on_NewGame_mouse_entered():
+	$NewGame.color = Color.slategray
+
+func _on_NewGame_mouse_exited():
+	$NewGame.color = Color.gainsboro
+	
+func _on_Quit_gui_input(event):
+	if event is InputEventMouseButton:
+		get_tree().quit()
+
+func _on_Quit_mouse_entered():
+	$Quit.color = Color.slategray
+
+func _on_Quit_mouse_exited():
+	$Quit.color = Color.gainsboro
+

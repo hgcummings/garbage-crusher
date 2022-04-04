203756ee20866e794955e55197c33cf3e3c01839
SChandarana <shivamchandarana@gmail.com>
2022-04-04 15:49:56 +0100

    Can no longer become a black hole

--- Channel.gd
+++ Channel.gd
@@ -5,13 +5,13 @@ var contents
 
 export var layer_zero_indexed = 0
 const wall_layer = 8
+const interactable_layer = 12
 
 func _ready():
 	contents = self.get_node("Contents")
 
 func add_stick(stick):
 	var new_stick = stick.as_channel_stick() as KinematicBody2D
-	print(new_stick)
 
 	contents.add_child(new_stick)
 	
@@ -20,14 +20,13 @@ func add_stick(stick):
 	new_stick.global_position = stick.global_position
 	new_stick.global_position.y = self.global_position.y
 	
-	
-	
 	new_stick.collision_layer = 0
 	new_stick.collision_mask = 0
 	
 	new_stick.set_collision_layer_bit(layer_zero_indexed, true)
 	new_stick.set_collision_mask_bit(wall_layer, true)
 	new_stick.set_collision_mask_bit(layer_zero_indexed, true)
+	new_stick.set_collision_mask_bit(interactable_layer, true)
 	
 	var collision = new_stick.move_and_collide(Vector2.ZERO, true, true, true)
 	if collision:
